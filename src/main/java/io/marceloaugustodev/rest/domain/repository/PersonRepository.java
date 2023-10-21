47ee88bdd55ee022e34102624b827951314afe26
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2023-10-21 13:45:54 -0300

    Implementado UsuarioResponseDto para a Resquest findByID

--- src/main/java/io/marceloaugustodev/rest/domain/repository/PersonRepository.java
+++ src/main/java/io/marceloaugustodev/rest/domain/repository/PersonRepository.java
@@ -2,9 +2,8 @@ package io.marceloaugustodev.rest.domain.repository;
 
 import io.marceloaugustodev.rest.domain.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
 }
